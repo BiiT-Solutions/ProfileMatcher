106a73d5e4daa99f8ee506f13bf21d62e0bc5aeb
Jorge Hortelano <jhortelano@biit-solutions.com>
2024-09-11 11:46:07 +0200

    Update exceptions to include frontend codes

--- profile-matcher-rest/src/main/java/com/biit/profile/rest/exceptions/ProfileExceptionControllerAdvice.java
+++ profile-matcher-rest/src/main/java/com/biit/profile/rest/exceptions/ProfileExceptionControllerAdvice.java
@@ -5,10 +5,10 @@ import com.biit.profile.core.exceptions.CommentTooLongException;
 import com.biit.profile.core.exceptions.ProfileNotFoundException;
 import com.biit.profile.persistence.entities.ProfileCandidateComment;
 import com.biit.profile.persistence.entities.exceptions.InvalidProfileValueException;
+import com.biit.server.exceptions.ErrorResponse;
 import com.biit.server.exceptions.NotFoundException;
 import com.biit.server.exceptions.ServerExceptionControllerAdvice;
 import com.biit.server.logger.RestServerExceptionLogger;
-import org.modelmapper.spi.ErrorMessage;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -20,31 +20,31 @@ public class ProfileExceptionControllerAdvice extends ServerExceptionControllerA
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<Object> notFoundException(Exception ex) {
         RestServerExceptionLogger.errorMessage(this.getClass().getName(), ex);
-        return new ResponseEntity<>(new ErrorMessage("NOT_FOUND", ex), HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(new ErrorResponse("NOT_FOUND", ex), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(InvalidProfileValueException.class)
     public ResponseEntity<Object> invalidProfileValueException(Exception ex) {
         RestServerExceptionLogger.errorMessage(this.getClass().getName(), ex);
-        return new ResponseEntity<>(new ErrorMessage("Invalid Profile payload.", ex), HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(new ErrorResponse("Invalid Profile payload.", "malformed_profile", ex), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(ProfileNotFoundException.class)
     public ResponseEntity<Object> profileNotFoundException(Exception ex) {
         RestServerExceptionLogger.errorMessage(this.getClass().getName(), ex);
-        return new ResponseEntity<>(new ErrorMessage(ex.getMessage(), ex), HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(new ErrorResponse(ex.getMessage(), "profile_not_found", ex), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(CandidateNotFoundException.class)
     public ResponseEntity<Object> candidateNotFoundException(Exception ex) {
         RestServerExceptionLogger.errorMessage(this.getClass().getName(), ex);
-        return new ResponseEntity<>(new ErrorMessage(ex.getMessage(), ex), HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(new ErrorResponse(ex.getMessage(), "candidate_not_found", ex), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(CommentTooLongException.class)
     public ResponseEntity<Object> commentTooLongException(Exception ex) {
         RestServerExceptionLogger.errorMessage(this.getClass().getName(), ex);
-        return new ResponseEntity<>(new ErrorMessage("Comment length exceeds the limit of '"
-                + ProfileCandidateComment.COMMENT_LENGTH + "' bytes", ex), HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(new ErrorResponse("Comment length exceeds the limit of '"
+                + ProfileCandidateComment.COMMENT_LENGTH + "' bytes", "comment_too_long", ex), HttpStatus.BAD_REQUEST);
     }
 }
